d5ba4eefaf1e360c0921ceefdba97ce9e56d3bc0
112224 <didgns25@naver.com>
2022-06-21 05:09:46 +0900

    Revert "Singleton and CGLIB"
    This reverts commit 1b3aafd13f7b7e78ce69cdc707d97570bfbf2800.

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -31,12 +31,4 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
-
-    @Test
-    void configurationDeep() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-        AppConfig bean = ac.getBean(AppConfig.class);
-
-        System.out.println("bean = " + bean);
-    }
 }
